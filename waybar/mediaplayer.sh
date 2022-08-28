e03720cf6e462bc8d56541ff9845d3c120c6beca
catarch <alanqa-ops@outlook.com>
2022-08-28 11:59:43 +0800

    Hard Linked

--- waybar/mediaplayer.sh
+++ waybar/mediaplayer.sh
@@ -1,1 +1,7 @@
-/home/anxus/.config/waybar/mediaplayer.sh
+#!/bin/sh
+player_status=$(playerctl status 2> /dev/null)
+if [ "$player_status" = "Playing" ]; then
+    echo "$(playerctl metadata artist) - $(playerctl metadata title)"
+elif [ "$player_status" = "Paused" ]; then
+    echo " $(playerctl metadata artist) - $(playerctl metadata title)"
+fi
